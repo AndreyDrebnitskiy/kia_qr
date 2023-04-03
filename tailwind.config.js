b084061eda422f46d3d95a0a40982439dd15e16d
AndreyDrebnitskiy <awesome.j33@yandex.ru>
2023-04-03 15:46:14 +0400

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'black': '#020617',
+        'green': {
+            100: '#dcfce7',
+            200: '#bbf7d0',
+            300: '#86efac',
+            400: '#4ade80',
+            500: '#22c55e',
+            600: '#16a34a',
+            700: '#15803d',
+            800: '#166534',
+            900: '#14532d',
+          },
+        'yellow': {
+            100: '#fef9c3',
+            200: '#fef08a',
+            300: '#facc15',
+            400: '#facc15',
+            500: '#eab308',
+          },
+      },
+    },
+  }
